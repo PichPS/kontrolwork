907bfa9fcb37c674629fe9f8380433d2b5560ac9
Pavel_Pichugin <pichps@mail.ru>
2023-03-14 07:16:12 +0300

    Задача - финиш

--- Program.cs
+++ Program.cs
@@ -2,3 +2,18 @@
 // длина которых меньше либо равна 3 символа. Перваначальный массив можно ввести с клавиатуры
 // либо задать на старте выполнения алгоритма.
 // пример:["hello", "2", "world", "*)"] -> ["2", "*)"]
+
+string[] startArray = new string[] { "Kak", "Tvoi", "dela", "drug", "?" };
+string[] resultArray = new string[0];
+
+foreach (var item in startArray)
+{
+    if (item.Length <= 3)
+    {
+    Array.Resize(ref resultArray, resultArray.Length + 1);
+    resultArray [resultArray.Length-1] = item; 
+    }
+
+}
+
+Console.WriteLine($"[{String.Join("; ", startArray)}]->[{String.Join("; ", resultArray)}]");
